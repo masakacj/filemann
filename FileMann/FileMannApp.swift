edfd245df23eaf80cdf6ccd13bcdd5115572a623
masakacj <34531511+masakacj@users.noreply.github.com>
2026-09-22 19:38:58 +0800

    Add FileMann/FileMannApp.swift

---
+++ FileMann/FileMannApp.swift
@@ -0,0 +1,13 @@
+import SwiftUI
+
+@main
+struct FileMannApp: App {
+    @StateObject private var viewModel = ArchiveViewModel()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environmentObject(viewModel)
+        }
+    }
+}
